856757ba8d634ff6bc5ad48309bab32bbc85028c
Maria Vitória <mmariavitoria2116@gmail.com>
2025-09-12 21:11:59 -0300

    Update Pessoa.java

--- src/main/java/com/mycompany/pessoa/Pessoa.java
+++ src/main/java/com/mycompany/pessoa/Pessoa.java
@@ -1,11 +1,33 @@
 package com.mycompany.pessoa;
-
+//pessoa
 public class Pessoa {
-    String nome;
-    String endereco;
+    protected String nome;
+    protected String endereco;
+
+    public Pessoa(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
 
     public void apresentar() {
         System.out.println("Nome: " + nome);
         System.out.println("Endereço: " + endereco);
     }
+
+  //pessoa juridica
+    public class PessoaJuridica {
+        private String cnpj;
+        private String razaoSocial;
+
+        public PessoaJuridica(String cnpj, String razaoSocial) {
+            this.cnpj = cnpj;
+            this.razaoSocial = razaoSocial;
+        }
+
+        public void apresentarJuridica() {
+            System.out.println("Razão Social: " + razaoSocial);
+            System.out.println("CNPJ: " + cnpj);
+        }
+    }
 }
+
