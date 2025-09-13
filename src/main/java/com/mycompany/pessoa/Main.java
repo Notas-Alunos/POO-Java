4080cf8c207bdfc33c357efd422a579cf113a6b3
Maria Vitória <mmariavitoria2116@gmail.com>
2025-09-12 21:15:36 -0300

    Update Main.java

--- src/main/java/com/mycompany/pessoa/Main.java
+++ src/main/java/com/mycompany/pessoa/Main.java
@@ -1,14 +1,11 @@
 package com.mycompany.pessoa;
 public class Main {
-  public static void main(String[] args) {
-        PessoaFisica pf = new PessoaFisica("João", "Rua A", "123.456.789-00");
-        pf.apresentar();
-        pf.mostrarCPF();
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Empresa X", "Av. Central");
+        pessoa.apresentar();
 
-        System.out.println();
-
-        PessoaJuridica pj = new PessoaJuridica("Empresa X", "Av. Central", "12.345.678/0001-99");
-        pj.apresentar();
-        pj.mostrarCNPJ();
+        // Criando a classe interna
+        Pessoa.PessoaJuridica pj = pessoa.new PessoaJuridica("12.345.678/0001-99", "Empresa X Ltda");
+        pj.apresentarJuridica();
     }
 }
