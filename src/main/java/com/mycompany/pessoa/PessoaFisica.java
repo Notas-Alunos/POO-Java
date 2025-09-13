c7c73ebeb3f6cd09a1a3f3026ace7665b0c01afb
Maria Vitória <mmariavitoria2116@gmail.com>
2025-09-12 21:12:17 -0300

    Update PessoaFisica.java

--- src/main/java/com/mycompany/pessoa/PessoaFisica.java
+++ src/main/java/com/mycompany/pessoa/PessoaFisica.java
@@ -1,16 +1,15 @@
 package com.mycompany.pessoa;
-
+//pessoa fisica
 public class PessoaFisica extends Pessoa {
     private String cpf;
 
     public PessoaFisica(String nome, String endereco, String cpf) {
-        this.nome = nome;
-        this.endereco = endereco;
+        super(nome, endereco); // Chama o construtor da classe Pessoa
         this.cpf = cpf;
+       
     }
 
     public void mostrarCPF() {
         System.out.println("CPF: " + cpf);
     }
 }
-
