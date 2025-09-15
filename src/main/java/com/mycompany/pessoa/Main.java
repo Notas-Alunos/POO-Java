9f7cdf31b0daf1bdf86bf3d93d2fd0e8edbc3e0d
Maria Vitória <mmariavitoria2116@gmail.com>
2025-09-15 20:20:23 -0300

    Update Main.java

--- src/main/java/com/mycompany/pessoa/Main.java
+++ src/main/java/com/mycompany/pessoa/Main.java
@@ -1,11 +1,21 @@
 package com.mycompany.pessoa;
+
 public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa("Empresa X", "Av. Central");
         pessoa.apresentar();
-
-        // Criando a classe interna
+        
         Pessoa.PessoaJuridica pj = pessoa.new PessoaJuridica("12.345.678/0001-99", "Empresa X Ltda");
+        pj.apresentarJuridica();
+
+        pj.setCnpj("98.765.432/0001-00");
+        pj.setRazaoSocial("Nova Empresa S.A.");
+        
+        System.out.println("\n--- Dados Atualizados ---");
+        System.out.println("Novo CNPJ: " + pj.getCnpj());
+        System.out.println("Nova Razão Social: " + pj.getRazaoSocial());
+
+        System.out.println("\n--- Apresentação atualizada ---");
         pj.apresentarJuridica();
     }
 }
